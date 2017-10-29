5511c06f9534dbbf42485bcddf4805bcda9eb26f
sinnedh <dennis.hellmich@me.com>
2017-10-29 21:20:09 +0100

    Handla paid_at in tests

--- webclient/src/components/ExpenseList.test.js
+++ webclient/src/components/ExpenseList.test.js
@@ -10,8 +10,8 @@ it('renders without expenses', () => {
 it('renders with expenses', () => {
   const div = document.createElement('div');
   const expenses = [
-    { description: "Pasta", amount: 800, paid_for: [], paid_by: [], },
-    { description: "Pizza", amount: 1200, paid_for: [], paid_by: [], },
+    { description: "Pasta", amount: 800, paid_for: [], paid_by: [], paid_at: "2017-11-11 15:28"},
+    { description: "Pizza", amount: 1200, paid_for: [], paid_by: [], paid_at: "2016-09-14 12:18"},
   ]
   ReactDOM.render(<ExpenseList expenses={expenses} />, div);
 });
