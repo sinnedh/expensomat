4d42f5e1a5018044736e1aa2a755473af1f76fa4
sinnedh <dennis.hellmich@me.com>
2017-11-14 16:33:25 +0100

    Create formatAmount utility function

--- webclient/src/components/ExpenseListItem.js
+++ webclient/src/components/ExpenseListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {formatDate} from '../utils';
+import {formatAmount} from '../utils';
 
 export default (props) => (
     <li>
-      {formatDate(props.expense.paid_at)}&nbsp;
-      <strong>{props.expense.description}</strong>:&nbsp;
-      {props.expense.amount / 100} Euro
-      paid by {props.expense.paid_by.map((m, i) => <span key={i}>{m.name}</span>)}
-      &nbsp;for {props.expense.paid_for.map((m, i) => <span key={i}>{m.name}</span>)}
+      {formatDate(props.expense.paid_at)}:
+      { } <strong>{props.expense.description}</strong>
+      { } {formatAmount(props.expense.amount)}
+      { } paid by {props.expense.paid_by.map((m, i) => <span key={i}>{m.name}</span>)}
+      { } for {props.expense.paid_for.map((m, i) => <span key={i}>{m.name}</span>)}
     </li>
 )
