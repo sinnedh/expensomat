47f3e85a97b47f7d94c35f062381df83952f15e0
sinnedh <dennis.hellmich@me.com>
2017-11-20 21:24:57 +0100

    Rename existing calculation component to be more verbose

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Calculation from './routes/Calculation'
+import ShowCalculation from './routes/ShowCalculation';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Route path="/calculation/:token" component={Calculation} />
+          <Route path="/calculation/:token" component={ShowCalculation} />
         </div>
       </Router>
     );
