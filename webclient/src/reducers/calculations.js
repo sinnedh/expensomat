09a0f955839fa3b3727421f88c997e2117c6490e
sinnedh <dennis.hellmich@me.com>
2017-11-26 11:23:55 +0100

    Create reducers for loading calculation details

--- webclient/src/reducers/calculations.js
+++ webclient/src/reducers/calculations.js
@@ -1,10 +1,31 @@
 const initialState = {
+  name: '',
+  description: '',
+  members: [],
+  matrix: {},
   expenses: [],
   isFetching: false,
 }
 
 const calculations = (state = initialState, action) => {
   switch (action.type) {
+    case 'CALCULATION:LOAD_REQUEST':
+      return {...state, isFetching: true};
+
+    case 'CALCULATION:LOAD_SUCCESS':
+      return {
+        ...state,
+        name: action.name,
+        description: action.description,
+        members: action.members,
+        matrix: action.matrix,
+        isFetching: false
+      };
+
+    case 'CALCULATION:LOAD_FAILURE':
+      return {...state, isFetching: false};
+
+
     case 'CALCULATION:LOAD_EXPENSES_REQUEST':
       return {...state, isFetching: true};
 
--- webclient/src/reducers/calculations.test.js
+++ webclient/src/reducers/calculations.test.js
@@ -50,3 +50,61 @@ describe('CALCULATION:LOAD_EXPENSES_SUCCESS', () => {
     expect(calculations(beforeState, action(expenses))).toEqual(afterState);
   });
 });
+
+
+
+const initialState = {name: '', description: '', matrix: [], members: []};
+
+describe('CALCULATION:LOAD_REQUEST', () => {
+  const action = {type: 'CALCULATION:LOAD_REQUEST'};
+
+  it('sets isFetching', () => {
+    const beforeState = {...initialState, isFetching: false};
+    const afterState = {...initialState, isFetching: true};
+
+    expect(calculations(beforeState, action)).toEqual(afterState);
+  });
+});
+
+describe('CALCULATION:LOAD_FAILURE', () => {
+  const action = {type: 'CALCULATION:LOAD_FAILURE'};
+
+  it('resets isFetching', () => {
+    const beforeState = {...initialState, isFetching: true};
+    const afterState = {...initialState, isFetching: false};
+
+    expect(calculations(beforeState, action)).toEqual(afterState);
+  });
+
+  it('does not change the calculation', () => {
+    const calculation = {name: 'Calculation', description: 'bla', matrix: [], members: []};
+
+    const beforeState = {...calculation, isFetching: true};
+    const afterState = {...calculation, isFetching: false};
+
+    expect(calculations(beforeState, action)).toEqual(afterState);
+  });
+});
+
+describe('CALCULATION:LOAD_SUCCESS', () => {
+  const action = (calculation) => ({
+    ...calculation,
+    type: 'CALCULATION:LOAD_SUCCESS',
+  });
+
+  it('resets isFetching', () => {
+    const beforeState = {...initialState, isFetching: true};
+    const afterState = {...initialState, isFetching: false};
+
+    expect(calculations(beforeState, action(initialState))).toEqual(afterState);
+  });
+
+  it('sets the expenses', () => {
+    const calculation = {name: 'Calculation', description: 'bla', matrix: [], members: []};
+
+    const beforeState = {...initialState, isFetching: true};
+    const afterState = {...calculation,  isFetching: false};
+
+    expect(calculations(beforeState, action(calculation))).toEqual(afterState);
+  });
+});
