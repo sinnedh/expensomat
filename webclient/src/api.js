05da16db1efa163504b3a30d1a769ae246102f59
sinnedh <dennis.hellmich@me.com>
2017-11-28 11:05:14 +0100

    Include loading counter actions to api
    This will become much cleaner and more decoupled when switching to
    `redux-saga` instead of `redux-thunk`.

--- webclient/src/api.js
+++ webclient/src/api.js
@@ -1,13 +1,24 @@
 import 'whatwg-fetch';
+import {incrementLoadingCounter, decrementLoadingCounter} from './actions';
 
 const baseurl = 'http://localhost:4000/api';
 
+const requestSuccess = (dispatch, callback, data) => {
+  dispatch(callback(data));
+  dispatch(decrementLoadingCounter());
+}
+
+const requestFailure = (dispatch, callback, data) => {
+  dispatch(callback(data));
+  dispatch(decrementLoadingCounter());
+}
 
 const getRequest = (url, dispatch, onSuccess, onFailure) => {
+  dispatch(incrementLoadingCounter());
   fetch(url)
     .then(r => r.json())
-    .then(r => dispatch(onSuccess(r.data)))
-    .catch(e => dispatch(onFailure(e)));
+    .then(r => requestSuccess(dispatch, onSuccess, r.data))
+    .catch(e => requestFailure(dispatch, onFailure, e));
 }
 
 const postRequest = (url, body, dispatch, onSuccess, onFailure) => {
@@ -16,11 +27,11 @@ const postRequest = (url, body, dispatch, onSuccess, onFailure) => {
     headers: { 'Content-Type': 'application/json' },
     body,
   }
-
+  dispatch(incrementLoadingCounter());
   fetch(url, options)
     .then(r => r.json())
-    .then(r => onSuccess(r.data))
-    .catch(e => onFailure(e));
+    .then(r => requestSuccess(dispatch, onSuccess, r.data))
+    .catch(e => requestFailure(dispatch, onFailure, e));
 }
 
 export const addCalculation = (dispatch, calculation, onSuccess, onFailure) => {
