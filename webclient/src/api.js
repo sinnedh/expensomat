c21802d4fa51fee09cd1657ad6af5cc1a03910c0
sinnedh <dennis.hellmich@me.com>
2017-12-05 12:06:06 +0100

    Simplify api

--- webclient/src/api.js
+++ webclient/src/api.js
@@ -2,88 +2,47 @@ import 'whatwg-fetch';
 
 const baseurl = 'http://localhost:4000/api';
 
-const requestSuccess = (dispatch, callback, data) => {
-  dispatch(callback(data));
-}
-
-const requestFailure = (dispatch, callback, data) => {
-  dispatch(callback(data));
-}
-
-const request = (method, url, dispatch, onSuccess, onFailure, body) => {
+const applyRequest = (method, url, body) => {
   let options = {
     method: method,
     headers: { 'Content-Type': 'application/json' },
-  };
+  }
+
   if (body !== undefined) {
     options.body = body;
   }
-
-  fetch(url, options)
-    .then(res => res.text())
-    .then(text => text.length > 0 ? JSON.parse(text) : {})
-    .then(json => requestSuccess(dispatch, onSuccess, json.data))
-    .catch(e => requestFailure(dispatch, onFailure, e));
-}
-
-const postRequest = (url, dispatch, onSuccess, onFailure, body) => {
-  return request('POST', url, dispatch, onSuccess, onFailure, body);
-}
-
-const getRequest = (url, dispatch, onSuccess, onFailure) => {
-  return request('GET', url, dispatch, onSuccess, onFailure);
-}
-
-const deleteRequest = (url, dispatch, onSuccess, onFailure) => {
-  return request('DELETE', url, dispatch, onSuccess, onFailure, {});
-}
-
-export const addCalculation = (dispatch, calculation, onSuccess, onFailure) => {
-  postRequest(
-    `${baseurl}/calculations/`,
-    dispatch,
-    onSuccess,
-    onFailure,
-    JSON.stringify({ calculation }),
-  )
-};
-
-export const fetchCalculation = (token) => {
-  const options = {
-    method: 'GET',
-    headers: { 'Content-Type': 'application/json' },
-  }
-  const url = `${baseurl}/calculations/${token}`
 
   return fetch(url, options)
     .then(res => res.text())
     .then(text => text.length > 0 ? JSON.parse(text) : {})
 }
 
-export const fetchExpenses = (dispatch, token, onSuccess, onFailure) => {
-  getRequest(
-    `${baseurl}/calculations/${token}/expenses`,
-    dispatch,
-    onSuccess,
-    onFailure,
-  )
-};
+const postRequest = (url, body) => ( applyRequest('POST', url, body) )
 
-export const addExpense = (dispatch, token, expense, onSuccess, onFailure) => {
-  postRequest(
-    `${baseurl}/calculations/${token}/expenses`,
-    dispatch,
-    onSuccess,
-    onFailure,
-    JSON.stringify({ expense }),
-  )
-}
+const getRequest = (url) => ( applyRequest('GET', url) )
 
-export const deleteExpense = (dispatch, token, expense, onSuccess, onFailure) => {
-  deleteRequest(
-    `${baseurl}/calculations/${token}/expenses/${expense.id}`,
-    dispatch,
-    onSuccess,
-    onFailure,
-  )
-}
+const deleteRequest = (url) => ( applyRequest('DELETE', url) )
+
+export const createCalculation = (calculation) => (
+  postRequest(`${baseurl}/calculations/`, JSON.stringify({ calculation }))
+)
+
+export const fetchCalculation = (token) => (
+  getRequest(`${baseurl}/calculations/${token}`)
+)
+
+export const deleteCalculation = (token) => (
+  deleteRequest(`${baseurl}/calculations/${token}`)
+)
+
+export const fetchExpenses = (token) => (
+  getRequest(`${baseurl}/calculations/${token}/expenses`)
+)
+
+export const createExpense = (token, expense) => (
+  postRequest(`${baseurl}/calculations/${token}/expenses`, JSON.stringify({ expense }))
+)
+
+export const deleteExpense = (token, expenseId) => (
+  deleteRequest(`${baseurl}/calculations/${token}/expenses/${expenseId}`)
+)
