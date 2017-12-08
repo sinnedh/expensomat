728aaad10ef8f12b51239e70b2acd14ef6e878f8
sinnedh <dennis.hellmich@me.com>
2017-12-08 16:11:53 +0100

    Use context to pass token to components

--- webclient/src/components/App.js
+++ webclient/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CreateCalculation from '../routes/CreateCalculation';
 import ShowCalculation from '../routes/ShowCalculation';
@@ -7,18 +8,32 @@ import LoadingIndicator from './LoadingIndicator';
 import NotificationMessage from './NotificationMessage';
 import Navigation from './Navigation';
 
-export default (props) => (
-  <Router>
-    <div className="App">
-      <Navigation token={props.token} />
-      <LoadingIndicator isLoading={props.isLoading} />
-      <NotificationMessage
-        text={props.notificationText}
-        type={props.notificationType}
-      />
-      <Route exact path="/calculation" component={CreateCalculation} />
-      <Route exact path="/calculation/:token/manage" component={ManageCalculation} />
-      <Route exact path="/calculation/:token" component={ShowCalculation} />
-    </div>
-  </Router>
-)
+class App extends React.Component {
+  getChildContext() {
+    return {token: this.props.token};
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navigation token={this.props.token} />
+          <LoadingIndicator isLoading={this.props.isLoading} />
+          <NotificationMessage
+            text={this.props.notificationText}
+            type={this.props.notificationType}
+          />
+          <Route exact path="/calculation" component={CreateCalculation} />
+          <Route exact path="/calculation/:token/manage" component={ManageCalculation} />
+          <Route exact path="/calculation/:token" component={ShowCalculation} />
+        </div>
+      </Router>
+    )
+  }
+}
+
+App.childContextTypes = {
+  token: PropTypes.string
+}
+
+export default App
--- webclient/src/components/Navigation.js
+++ webclient/src/components/Navigation.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-export default (props) => {
+const Navigation = (props, context) => {
   return (
     <ul>
       <li><Link to="/calculation">Create new calculation</Link></li>
-      <li><Link to={`/calculation/${props.token}`}>Show calculation</Link></li>
-      <li><Link to={`/calculation/${props.token}/manage`}>Manage calculation</Link></li>
+      <li><Link to={`/calculation/${context.token}`}>Show calculation</Link></li>
+      <li><Link to={`/calculation/${context.token}/manage`}>Manage calculation</Link></li>
     </ul>
   )
 }
+
+Navigation.contextTypes = {token: PropTypes.string}
+
+export default Navigation
