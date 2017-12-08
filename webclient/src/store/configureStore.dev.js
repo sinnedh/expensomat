98e2f97dbd53566b2ecb203be2906e2fcbb5a7f2
sinnedh <dennis.hellmich@me.com>
2017-12-08 12:12:05 +0100

    Extract store creation and distinguish prod and dev

---
+++ webclient/src/store/configureStore.dev.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducers from '../reducers'
+import sagas from '../sagas'
+
+export default function configureStore(initialState) {
+  const sagaMiddleware = createSagaMiddleware()
+  const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(sagaMiddleware)),
+  )
+
+  sagaMiddleware.run(sagas)
+
+  return store
+}
