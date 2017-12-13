0e9007e63c21bbc4ef570445811deb5b4919e41f
sinnedh <dennis.hellmich@me.com>
2017-12-13 11:59:56 +0100

    Test utils.getFormFieldValue()

--- webclient/src/utils.test.js
+++ webclient/src/utils.test.js
@@ -1,4 +1,4 @@
-import { formatAmount, formatDate } from './utils'
+import { formatAmount, formatDate, getFormFieldValue } from './utils'
 
 describe('formatAmount', () => {
   it('renders positive values', () => {
@@ -27,5 +27,54 @@ describe('formatDate', () => {
     expect(formatDate('2017-11-11 15:28')).toEqual('2017-11-11')
     expect(formatDate('2017-11-11T11:00:00Z')).toEqual('2017-11-11')
     expect(formatDate('2017-11-11')).toEqual('2017-11-11')
+  })
+})
+
+describe('getFormFieldValue', () => {
+  it('returns true for checked checkbox', () => {
+    const target = {type: 'checkbox', checked: true}
+    expect(getFormFieldValue(target)).toEqual(true)
+  })
+
+  it('returns true false for unchecked checkbox', () => {
+    const target = {type: 'checkbox', checked: false}
+    expect(getFormFieldValue(target)).toEqual(false)
+  })
+
+  it('returns value for text input', () => {
+    const target = {type: 'input', value: 'Humpty'}
+    expect(getFormFieldValue(target)).toEqual('Humpty')
+  })
+
+  it('returns value for textarea', () => {
+    const target = {type: 'textarea', value: 'This is a long text'}
+    expect(getFormFieldValue(target)).toEqual('This is a long text')
+  })
+
+  it('returns value for select', () => {
+    const target = {type: 'select', value: '73215'}
+    expect(getFormFieldValue(target)).toEqual('73215')
+  })
+
+  it('returns array with selected options for multiple-select', () => {
+    const options = [
+      {value: 121, selected: true},
+      {value: 324, selected: false},
+      {value: 3, selected: true},
+      {value: 478, selected: false}
+    ]
+    const target = {type: 'select-multiple', options}
+    expect(getFormFieldValue(target)).toEqual([121, 3])
+  })
+
+  it('returns empty array for multiple-select without selected options', () => {
+    const options = [
+      {value: 121, selected: false},
+      {value: 324, selected: false},
+      {value: 3, selected: false},
+      {value: 478, selected: false}
+    ]
+    const target = {type: 'select-multiple', options}
+    expect(getFormFieldValue(target)).toEqual([])
   })
 })
