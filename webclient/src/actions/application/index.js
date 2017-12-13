a5408c3a16f7053d688c9e3ce5e67b11be31d46f
sinnedh <dennis.hellmich@me.com>
2017-12-13 15:47:38 +0100

    Move token actions to separate file

--- webclient/src/actions/application/index.js
+++ webclient/src/actions/application/index.js
@@ -1,9 +1,3 @@
 export * from './loadingCounter'
 export * from './notifications'
-
-export const setToken = (token) => {
-  return {
-    type: 'APPLICATION:SET_TOKEN',
-    token,
-  }
-}
+export * from './token'
