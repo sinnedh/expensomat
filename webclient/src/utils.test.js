d7d660af4d6a0b295437b58e1fc7c6312a468201
sinnedh <dennis.hellmich@me.com>
2017-12-13 09:03:14 +0100

    Test utils.formatDate()

--- webclient/src/utils.test.js
+++ webclient/src/utils.test.js
@@ -1,4 +1,4 @@
-import {formatAmount} from './utils'
+import { formatAmount, formatDate } from './utils'
 
 describe('formatAmount', () => {
   it('renders positive values', () => {
@@ -19,5 +19,13 @@ describe('formatAmount', () => {
   it('renders negative values less than one 100', () => {
     const formattedAmount = formatAmount(-19)
     expect(formattedAmount).toEqual('-0.19 Euro')
+  })
+})
+
+describe('formatDate', () => {
+  it('renders date with different input strings', () => {
+    expect(formatDate('2017-11-11 15:28')).toEqual('2017-11-11')
+    expect(formatDate('2017-11-11T11:00:00Z')).toEqual('2017-11-11')
+    expect(formatDate('2017-11-11')).toEqual('2017-11-11')
   })
 })
