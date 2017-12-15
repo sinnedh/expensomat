e50ad9e94eb644b22d0fcc6e7f9a1aead940b3b8
sinnedh <dennis.hellmich@me.com>
2017-12-15 17:24:49 +0100

    Transfer application reducers to Immutable datastructures

--- webclient/src/reducers/application.js
+++ webclient/src/reducers/application.js
@@ -1,40 +1,35 @@
-const initialState = {
+import { Map } from 'immutable'
+
+const initialState = Map({
   loadingCounter: 0,
   notificationText: null,
   notificationType: null,
   token: null,
-}
+})
 
 export default (state = initialState, action) => {
   switch (action.type) {
 
     case 'APPLICATION:SET_TOKEN':
-      return {
-        ...state,
-        token: action.token,
-      }
+      return state.update('token', token => action.token)
 
     case 'APPLICATION:INCREMENT_LOADING_COUNTER':
-      return {...state, loadingCounter: state.loadingCounter + 1};
+      return state.update('loadingCounter', l => l + 1)
 
     case 'APPLICATION:DECREMENT_LOADING_COUNTER':
-      return {
-        ...state,
-        loadingCounter: state.loadingCounter > 0 ? state.loadingCounter - 1 : 0,
-      };
+      return state.update('loadingCounter', l => l > 0 ? l - 1 : 0)
 
     case 'APPLICATION:RESET_LOADING_COUNTER':
-      return {...state, loadingCounter: 0};
+      return state.update('loadingCounter', _ => 0)
 
     case 'APPLICATION:NOTIFICATION_RESET':
       return initialState;
 
     case 'APPLICATION:NOTIFICATION_SET':
-      return {
-        ...state,
+      return state.merge({
         notificationText: action.notificationText,
         notificationType: action.notificationType,
-      }
+      })
 
     default:
       return state;
--- webclient/src/reducers/application.test.js
+++ webclient/src/reducers/application.test.js
@@ -1,19 +1,20 @@
+import { Map } from 'immutable'
 import application from './application';
 
-const initialState = {
+const initialState = Map({
   loadingCounter: 0,
   notificationText: null,
   notificationType: null,
   token: null,
-};
+});
 
 describe('APPLICATION:SET_TOKEN', () => {
   const token = 'ABCD1234';
   const action = {type: 'APPLICATION:SET_TOKEN', token};
 
   it('increments counter when is 0', () => {
-    const beforeState = {...initialState};
-    const afterState = {...initialState, token};
+    const beforeState = initialState
+    const afterState = initialState.update('token', t => token)
 
     expect(application(beforeState, action)).toEqual(afterState);
   });
@@ -23,15 +24,15 @@ describe('APPLICATION:INCREMENT_LOADING_COUNTER', () => {
   const action = {type: 'APPLICATION:INCREMENT_LOADING_COUNTER'};
 
   it('increments counter when is 0', () => {
-    const beforeState = {...initialState, loadingCounter: 0};
-    const afterState = {...initialState, loadingCounter: 1};
+    const beforeState = initialState.update('loadingCounter', _ => 0)
+    const afterState = initialState.update('loadingCounter', _ => 1)
 
     expect(application(beforeState, action)).toEqual(afterState);
   });
 
   it('increments counter when is larger than 0', () => {
-    const beforeState = {...initialState, loadingCounter: 55};
-    const afterState = {...initialState, loadingCounter: 56};
+    const beforeState = initialState.update('loadingCounter', _ => 55)
+    const afterState = initialState.update('loadingCounter', _ => 56)
 
     expect(application(beforeState, action)).toEqual(afterState);
   });
@@ -41,22 +42,22 @@ describe('APPLICATION:DECREMENT_LOADING_COUNTER', () => {
   const action = {type: 'APPLICATION:DECREMENT_LOADING_COUNTER'};
 
   it('does not decrement counter when is 0', () => {
-    const beforeState = {...initialState, loadingCounter: 0};
-    const afterState = {...initialState, loadingCounter: 0};
+    const beforeState = initialState.update('loadingCounter', _ => 0)
+    const afterState = initialState.update('loadingCounter', _ => 0)
 
     expect(application(beforeState, action)).toEqual(afterState);
   });
 
   it('decrements counter to 0 when is 1', () => {
-    const beforeState = {...initialState, loadingCounter: 1};
-    const afterState = {...initialState, loadingCounter: 0};
+    const beforeState = initialState.update('loadingCounter', _ => 1)
+    const afterState = initialState.update('loadingCounter', _ => 0)
 
     expect(application(beforeState, action)).toEqual(afterState);
   });
 
   it('decrements counter when is larger than 0', () => {
-    const beforeState = {...initialState, loadingCounter: 55};
-    const afterState = {...initialState, loadingCounter: 54};
+    const beforeState = initialState.update('loadingCounter', _ => 55)
+    const afterState = initialState.update('loadingCounter', _ => 54)
 
     expect(application(beforeState, action)).toEqual(afterState);
   });
@@ -66,15 +67,15 @@ describe('APPLICATION:RESET_LOADING_COUNTER', () => {
   const action = {type: 'APPLICATION:RESET_LOADING_COUNTER'};
 
   it('does reset counter when is 0', () => {
-    const beforeState = {...initialState, loadingCounter: 0};
-    const afterState = {...initialState, loadingCounter: 0};
+    const beforeState = initialState.update('loadingCounter', _ => 0)
+    const afterState = initialState.update('loadingCounter', _ => 0)
 
     expect(application(beforeState, action)).toEqual(afterState);
   });
 
   it('does reset counter when is larger than 0', () => {
-    const beforeState = {...initialState, loadingCounter: 55};
-    const afterState = {...initialState, loadingCounter: 0};
+    const beforeState = initialState.update('loadingCounter', _ => 55)
+    const afterState = initialState.update('loadingCounter', _ => 0)
 
     expect(application(beforeState, action)).toEqual(afterState);
   });
@@ -84,11 +85,10 @@ describe('APPLICATION:NOTIFICATION_RESET', () => {
   const action = {type: 'APPLICATION:NOTIFICATION_RESET'};
 
   it('resets to initial state', () => {
-    const beforeState = {
-      ...initialState,
+    const beforeState = initialState.merge({
       notificationText: 'Hello world',
       notificationType: 'something',
-    };
+    })
     const afterState = initialState;
 
     expect(application(beforeState, action)).toEqual(afterState);
@@ -97,22 +97,22 @@ describe('APPLICATION:NOTIFICATION_RESET', () => {
 
 describe('APPLICATION:NOTIFICATION_SET', () => {
   const testCases = [
-    {...initialState, type: 'error', text: 'Something went wrong.'},
-    {...initialState, type: 'warning', text: 'Something could go wrong.'},
-    {...initialState, type: 'success', text: 'Something succeeded.'},
-    {...initialState, type: 'info', text: 'Something happened.'},
+    initialState.merge({type: 'error', text: 'Something went wrong.'}),
+    initialState.merge({type: 'warning', text: 'Something could go wrong.'}),
+    initialState.merge({type: 'success', text: 'Something succeeded.'}),
+    initialState.merge({type: 'info', text: 'Something happened.'}),
   ];
 
   for (const testCase of testCases) {
-    it(testCase.type.toUpperCase() + ' sets text and type', () => {
+    it(testCase.get('type').toUpperCase() + ' sets text and type', () => {
       const action = {
         type: 'APPLICATION:NOTIFICATION_SET',
-        notificationType: testCase.type,
-        notificationText: testCase.text,
+        notificationType: testCase.get('type'),
+        notificationText: testCase.get('text'),
       };
 
-      const beforeState = {...initialState, notificationText: 'Hello world', notificationType: 'something'};
-      const afterState = {...initialState, notificationText: testCase.text, notificationType: testCase.type};
+      const beforeState = initialState.merge({notificationText: 'Hello world', notificationType: 'something'});
+      const afterState = initialState.merge({notificationText: testCase.get('text'), notificationType: testCase.get('type')});
 
       expect(application(beforeState, action)).toEqual(afterState);
     });
