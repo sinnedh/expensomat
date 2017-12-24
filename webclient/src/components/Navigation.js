e1c670e04c53eb86d049ab146fc70f9b23315160
sinnedh <dennis.hellmich@me.com>
2017-12-24 21:58:08 +0100

    Show navigation only when token is set

--- webclient/src/components/Navigation.js
+++ webclient/src/components/Navigation.js
@@ -3,13 +3,16 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
 const Navigation = (props, context) => {
-  return (
-    <ul>
-      <li><Link to="/calculation">Create new calculation</Link></li>
-      <li><Link to={`/calculation/${props.token}`}>Show calculation</Link></li>
-      <li><Link to={`/calculation/${props.token}/manage`}>Manage calculation</Link></li>
-    </ul>
-  )
+  if (props.token) {
+    return (
+      <ul>
+        <li><Link to={`/calculation/${props.token}`}>Show calculation</Link></li>
+        <li><Link to={`/calculation/${props.token}/manage`}>Manage calculation</Link></li>
+      </ul>
+    )
+  } else {
+    return <ul></ul>
+  }
 }
 
 Navigation.contextTypes = {token: PropTypes.string}
